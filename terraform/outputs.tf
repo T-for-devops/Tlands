22ab542591bdfb213b5d7a7255e74ab93d387f37
T-for-devops <bly2chtrdrg@gmail.com>
2026-01-09 11:39:48 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,6 @@
-output "eks_cluster_name" {
-  value = aws_eks_cluster.this.name
-}
+#output "eks_cluster_name" {
+ # value = aws_eks_cluster.this.name
+#}
 
 output "ecr_repo_url" {
   value = aws_ecr_repository.app.repository_url
